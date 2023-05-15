1b1eb799abb0fe4419013f28066796f5953006f8
carozum <czumbiehlfaure@hotmail.fr>
2023-05-15 16:16:38 +0200

    adds sketch2.js with objects definition

--- visualJS/sketch.js
+++ visualJS/sketch.js
@@ -1,9 +1,11 @@
 // using p5.js
-var count=0;
+var toggle = true;
+
 
 function setup() {
     createCanvas(400, 1200);
     rectMode(CENTER);
+    textAlign(CENTER, CENTER);
 }
 
 function draw() {
@@ -35,14 +37,36 @@ function draw() {
 
     // drawing a stop
     var xStop = x;
-    var dStop = 100 + count;
+    var dStop = 100 + frameCount;
     var yStop = y + d/2 + dStop/2 +50;
     fill(230, 50, 50);
     ellipse(xStop, yStop, dStop, dStop);
     fill(255);
     rect(xStop, yStop, dStop *0.75, dStop * 0.13);
 
-    // first animation
-    count = count +1;
+    // writing stop when frameCount is less than 200
+    fill(0);
+    var value;
+    if (frameCount <= 200){
+        textSize(36);
+        value='Small';
+    } else {
+        textSize(58);
+        value='Big'
+    }
+    text(value, xStop, yStop);
 
+    //adding mouse pressed event
+    if (mouseIsPressed){
+        toggle = !toggle;
+    }
+    if (toggle === true){
+        text('Hello', x, y);
+    }
+
+    // interaction with mouse position
+
+    var xA = mouseX;
+    var yA = mouseY;
+    ellipse(xA, yA, 50, 50);
 }
---
+++ visualJS/sketch2.js
@@ -0,0 +1,58 @@
+var circle;
+var aSquare;
+
+function setup(){
+    createCanvas(800, 300);
+    rectMode(CENTER);
+    circle ={
+        x : width/2,
+        y: height/2,
+        size : 150,
+        drawCircle: function(){
+            ellipse(this.x, this.y, this.size, this.size);
+        }
+    };
+
+    aSquare = new Square();
+}
+
+function draw(){
+    background(250, 4, 67);
+    fill(30, 140, 30);
+    // noStroke();
+    var diameter = 50;
+    for (var i=0 ; i< width/diameter ; i++){
+        for (var j= 0; j <= height/2 ; j ++){
+            ellipse(
+                i * diameter +diameter/2, 
+                j * diameter + diameter / 2, 
+                diameter * noise(frameCount /100 + i + j), 
+                diameter * noise(frameCount /100 + j + i));
+        } 
+    }
+
+    // play with objects
+    fill(200, 200, 0);
+    circle.drawCircle();
+
+    // play with object constructed using constructor
+    aSquare.drawSquare();
+    aSquare.grow();
+    
+}
+
+// constructor function
+function Square(){
+    this.x = width/3;
+    this.y = height/2;
+    this.size = 50;
+    this.drawSquare = function(){
+        fill(34, 34, 178);
+        rect(this.x, this.y, this.size, this.size);
+    }
+    this.grow = function(){
+        if (this.size < 200){
+            this.size = this.size +1;
+        }
+    }
+}
