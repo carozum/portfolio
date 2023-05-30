e65b0839debed8c88d74b825f11d07cea584ffd3
carozum <czumbiehlfaure@hotmail.fr>
2023-05-30 11:07:17 +0200

    creates a gluten app using crete-react-app

---
+++ reactGluten/gluten-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Gluten Application by Carozum
+      </header>
+    </div>
+  );
+}
+
+export default App;
